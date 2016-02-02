0a0cefe359f0c357de2943903f9ee31c492712da
FoxCarlos Trabajo <foxcarlos@gmail.com>
2016-02-02 15:37:18 -0430

    creada la funcion para desplegar el menu en el index con backbone.js

--- js/libs/main2.js
+++ js/libs/main2.js
@@ -10,7 +10,7 @@
     };
 
     // Models Index
-    App.Models.Person = Backbone.Model.extend({
+    App.Models.Index = Backbone.Model.extend({
         defaults:{
             cabeceraTitulo: 'EVENTOS ACADEMICOS E INVESTIGACION HOSPITAL COROMOTO',
             jumbotronTitulo: 'II JORNADAS CIENTIFICAS INTEGRALES DEL HOSPITAL COROMOTO',
@@ -24,62 +24,24 @@
         }
     });
 
-    //Lista de Personas
-    App.Collections.People = Backbone.Collection.extend({
-        model: App.Models.Person
-    });
-
-
-    //Vista para varias de Personas
-    App.Views.People = Backbone.View.extend({
-        tagName: 'ul',
-
-        initialize: function(){
-            console.log('metodo initialize de PeopleView');
-        },
-
-        render: function(){
-            this.collection.each(function(person){
-                var personaVista = new App.Views.Person({model: person});
-                this.$el.append(personaVista.render().el);
-            }, this);
-
-            return this;
-        }
-
-    });
-
-    //Vista para una persona
-    App.Views.Person = Backbone.View.extend({
-        tagName: 'li',
-
-        miPlantilla: plantilla('personaPlantilla'),  // _.template($('#personaPlantilla').html()) ,
-        /*initialize: function(){
-            this.render();
-        },*/
-
-        render: function(){
-            this.$el.html(this.miPlantilla(this.model.toJSON()));
-            return this;
-        }
-    });
-
     //Vista para el Index
     App.Views.Index = Backbone.View.extend({
         tagName: 'body',
 
         miPlantilla: plantilla('index'),  // _.template($('#personaPlantilla').html()) ,
+
         /*initialize: function(){
-            this.render();
+          $('#wrapper').toggleClass('toggled');
+          this.render();
         },*/
 
         events: {
-          'click #menu-toggle': 'miClick'
+          'click #menu-toggle': 'mostrarMenu'
         },
 
-        miClick: function(){
-          alert('Hizo clic ');
-          $(document.body).append('hola');   // adding people view in DOM.. Only for demo
+        mostrarMenu: function(e){
+          e.preventDefault()
+          this.$('#wrapper').toggleClass('toggled');
         },
 
         render: function(){
@@ -88,24 +50,7 @@
         }
     });
 
-    //Agregar datos a una coleccion
-    var peopleCollection = new App.Collections.People([
-            {
-                nombre: 'Carlos', edad: 43, ocupacion:'Developer'
-            },
-            {
-                nombre:'Nairesther', edad:26, ocupacion:'Educadora'
-            },
-            {
-                nombre: 'Paola', edad:15, ocupacion: 'Estudiante'
-            },
-            {
-                nombre: 'Carla', edad:6, ocupacion: 'Estudiante'
-            }
-            ]);
-
-    var persona = new App.Models.Person()
-    //var peopleView = new App.Views.People({ collection: peopleCollection });
-    var indexView = new App.Views.Index({model: persona});
+    var index = new App.Models.Index()
+    var indexView = new App.Views.Index({model: index});
     $(document.body).append(indexView.render().el);   // adding people view in DOM.. Only for demo
 })();
