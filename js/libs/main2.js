c55678497e0a05070fabc2a84784360e73932618
FoxCarlos Trabajo <foxcarlos@gmail.com>
2016-03-09 15:57:23 -0430

    se elimino la validacion de los datos del modelo y se paso a la vista

--- js/libs/main2.js
+++ js/libs/main2.js
@@ -6,7 +6,7 @@
     };
 
     window.plantilla = function(id){
-           return _.template( $('#' + id).html() );
+            return _.template( $('#' + id).html() );
     };
 
     // Models Index
@@ -48,297 +48,331 @@
             apellido: '',
             correo: '',
             clave: '',
+            clave2: '',
+            movil: '',
             fechaNac: '',
             genero: ''
         },
 
-        validate: function(attributes){
+        /*validate: function(attributes){
             if(attributes.nombre === ''){
-                return "En necesario indicar un nombre";
+                return "En necesario indicar un Nombre";
             }
-        },
+
+            if(attributes.apellido === ''){
+                return "En necesario indicar un Apellido";
+            }
+
+            if(attributes.correo === ''){
+                return "En necesario indicar un Correo Electronico";
+            }
+
+            if(attributes.clave === ''){
+                return "En necesario indicar uan Contraseña";
+            }
+
+            if(attributes.clave2 === ''){
+                return "En necesario que confirme su Contraseña";
+            }
+
+            if(attributes.clave != attributes.clave2){
+                return "La Contraseña y la contraseña de confirmacion no coinciden";
+            }
+
+            if(attributes.movil === ''){
+                return "En necesario indicar un Telefono Celular";
+            }
+
+            if(attributes.fechaNac === ''){
+                return "En necesario indicar su Fecha de Nacimiento";
+            }
+
+            if(attributes.genero === ''){
+                return "En necesario indicar un Genero";
+            }
+        },*/
 
         initialize: function(){
             this.on('invalid', function(model, error){
-                //alert(error);
             });
         }
-
     });
 
-    // Modelo para el Menu
-    App.Collections.Menu = Backbone.Collection.extend({
+// Modelo para el Menu
+App.Collections.Menu = Backbone.Collection.extend({
+    url: '/menu/1'
+    /*
+        Esta Coleccion se instancia:
+        var menu = new App.Collections.Menu();
+        menu.fetch()
+        luego se recorre con:
+        for(var i=0;i<menu.length;i=i+1){
+        console.log(menu.at(i).attributes)
+        }
+
+        menu.forEach(function(modelo, index, arreglo){
+        console.log(modelo.get('nombre'))
+        })
+
+        var Coleccion = Backbone.Collection.extend({
         url: '/menu/1'
-        /*
-         Esta Coleccion se instancia:
-         var menu = new App.Collections.Menu();
-         menu.fetch()
-         luego se recorre con:
-         for(var i=0;i<menu.length;i=i+1){
-            console.log(menu.at(i).attributes)
-         }
+});
 
-         menu.forEach(function(modelo, index, arreglo){
-           console.log(modelo.get('nombre'))
-           })
+miColeccion.forEach(function(modelo, index, arreglo){
+    if(modelo.get("depende_menu_id")==0){
+        console.log(modelo.get("nombre"));
+        $("#ulMenu").append('<li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">'+ modelo.get("nombre") +'<span class="caret"></span><a><ul '+'id="ul'+modelo.get("id")+'"'+' class="dropdown-menu" role="menu"></ul></li>')
+    }
+})
 
-         var Coleccion = Backbone.Collection.extend({
-          url: '/menu/1'
-    });
+    */
+});
 
-    miColeccion.forEach(function(modelo, index, arreglo){
-        if(modelo.get("depende_menu_id")==0){
-            console.log(modelo.get("nombre"));
-            $("#ulMenu").append('<li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">'+ modelo.get("nombre") +'<span class="caret"></span><a><ul '+'id="ul'+modelo.get("id")+'"'+' class="dropdown-menu" role="menu"></ul></li>')
+// Otra Prueba para generar menus:
+var VistaCabeceraMenues = Backbone.View.extend({
+    plantillaCabeceraMenues: plantilla('pru'),
+
+    initialize: function(){
+    this.render();
+    },
+
+    render: function(){
+    p = this.plantillaCabeceraMenues({nombreMenu: 'hola'});
+    console.log(p);
+    this.$el.html(p);
+    return this;
+    }
+});
+
+
+// Vista para el menu Configurar de la cabecera:
+var VistaCabeceraMenu = Backbone.View.extend({
+    plantillaCabeceraMenu: plantilla('pruebaPlantilla'),
+    plantillaCabeceraMenu2: plantilla('prueba2Plantilla'),
+
+    initialize: function(){
+    this.render();
+    },
+
+    render: function(){
+    suma = this.plantillaCabeceraMenu() + this.plantillaCabeceraMenu2();
+    console.log(suma);
+    this.$el.html(suma);
+    return this;
+    }
+});
+
+// Vista para la cabecera del Index:
+var VistaCabeceraIndex = Backbone.View.extend({
+    plantillaCabeceraIndex: plantilla('cabeceraIndexPlantilla'),
+
+    initialize: function(){
+    this.render();
+    },
+
+    events:{
+        'click #action': function(){alert('Hola')}
+    },
+
+    render: function(){
+    this.$el.html(this.plantillaCabeceraIndex(this.model.toJSON()));
+    return this;
+    }
+});
+
+// Vista para el Login del Index:
+var VistaLogin = Backbone.View.extend({
+    //plantillaLogin: plantilla('sesionActivaPlantilla'),
+    plantillaLogin: plantilla('sesionInactivaPlantilla'),
+
+
+    initialize: function(){
+    this.render();
+    },
+
+    render: function(){
+    this.$el.html(this.plantillaLogin());
+    return this;
+    }
+
+});
+
+// Vista para el Cuerpo del Index:
+var VistaCuerpoIndex = Backbone.View.extend({
+    plantillaCuerpoIndex: plantilla('cuerpoIndexPlantilla'),
+
+    initialize: function(){
+    this.render();
+    },
+
+    render: function(){
+    this.$el.html(this.plantillaCuerpoIndex(this.model.toJSON()));
+    return this;
+    }
+});
+
+// Vista para el Cuerpo del Index Parte 1:
+var VistaCuerpoIndexParte1 = Backbone.View.extend({
+    plantillaCuerpoIndexParte1: plantilla('cuerpoIndexParte1Plantilla'),
+
+    initialize: function(){
+    this.render();
+    },
+
+    render: function(){
+    this.$el.html(this.plantillaCuerpoIndexParte1(this.model.toJSON()));
+    return this;
+    }
+
+});
+
+// Vista para el Cuerpo del Index Parte 2:
+var VistaCuerpoIndexParte2 = Backbone.View.extend({
+    plantillaCuerpoIndexParte2: plantilla('cuerpoIndexParte2Plantilla'),
+    plantillaModal2: plantilla('modal2'),
+
+    initialize: function(){
+    this.render();
+    },
+
+    events:{
+        'click #botonRegistrar': 'registrarNuevo',
+    },
+
+    validarVacios: function(obj, obj2){
+        campo = $(obj).val();
+
+        if(!campo.trim()){
+            alert(obj2);
+            $(obj).focus();
         }
-   })
+    },
 
-        */
-    });
+    registrarNuevo: function(){
+        alert('Se regsitro');
 
-    // Otra Prueba para generar menus:
-    var VistaCabeceraMenues = Backbone.View.extend({
-      plantillaCabeceraMenues: plantilla('pru'),
+        nom = $('#txtnombre').val();
+        ape = $('#txtapellido').val();
+        corr = $('#txtcorreo').val();
+        cla = $('#newpassword').val();
+        cla2 = $('#newpassword2').val();
+        mov = $('#txtmovil').val();
+        fnac = $('#txtfechanac').val();
+        gen = $('input[type=radio]:checked').val();
 
-      initialize: function(){
-        this.render();
-      },
+        mo = new App.Models.RegistroRapido({
+            nombre: nom,
+            apellido: ape,
+            correo: corr,
+            clave: cla,
+            clave2: cla2,
+            movil: mov,
+            fechaNac: fnac,
+            genero: gen
+        });
+        r = mo.save();
 
-      render: function(){
-        p = this.plantillaCabeceraMenues({nombreMenu: 'hola'});
-        console.log(p);
-        this.$el.html(p);
-        return this;
-      }
-    });
+        // Recorreo todos los objetos tipo Input
+        longitud = $("#registroRapido :input").length;
+        lista = $("#registroRapido :input");  // lista todos los elementos input del form
 
-
-    // Vista para el menu Configurar de la cabecera:
-    var VistaCabeceraMenu = Backbone.View.extend({
-      plantillaCabeceraMenu: plantilla('pruebaPlantilla'),
-      plantillaCabeceraMenu2: plantilla('prueba2Plantilla'),
-
-      initialize: function(){
-        this.render();
-      },
-
-      render: function(){
-        suma = this.plantillaCabeceraMenu() + this.plantillaCabeceraMenu2();
-        console.log(suma);
-        this.$el.html(suma);
-        return this;
-      }
-    });
-
-    // Vista para la cabecera del Index:
-    var VistaCabeceraIndex = Backbone.View.extend({
-      plantillaCabeceraIndex: plantilla('cabeceraIndexPlantilla'),
-
-      initialize: function(){
-        this.render();
-      },
-
-      events:{
-          'click #action': function(){alert('Hola')}
-      },
-
-      render: function(){
-        this.$el.html(this.plantillaCabeceraIndex(this.model.toJSON()));
-        return this;
-      }
-    });
-
-    // Vista para el Login del Index:
-    var VistaLogin = Backbone.View.extend({
-        //plantillaLogin: plantilla('sesionActivaPlantilla'),
-        plantillaLogin: plantilla('sesionInactivaPlantilla'),
-
-
-      initialize: function(){
-        this.render();
-      },
-
-      render: function(){
-        this.$el.html(this.plantillaLogin());
-        return this;
-      }
-
-    });
-
-    // Vista para el Cuerpo del Index:
-    var VistaCuerpoIndex = Backbone.View.extend({
-      plantillaCuerpoIndex: plantilla('cuerpoIndexPlantilla'),
-
-      initialize: function(){
-        this.render();
-      },
-
-      render: function(){
-        this.$el.html(this.plantillaCuerpoIndex(this.model.toJSON()));
-        return this;
-      }
-    });
-
-    // Vista para el Cuerpo del Index Parte 1:
-    var VistaCuerpoIndexParte1 = Backbone.View.extend({
-      plantillaCuerpoIndexParte1: plantilla('cuerpoIndexParte1Plantilla'),
-
-      initialize: function(){
-        this.render();
-      },
-
-      render: function(){
-        this.$el.html(this.plantillaCuerpoIndexParte1(this.model.toJSON()));
-        return this;
-      }
-
-    });
-
-    // Vista para el Cuerpo del Index Parte 2:
-    var VistaCuerpoIndexParte2 = Backbone.View.extend({
-      plantillaCuerpoIndexParte2: plantilla('cuerpoIndexParte2Plantilla'),
-      plantillaModal2: plantilla('modal2'),
-
-      initialize: function(){
-        this.render();
-      },
-
-      events:{
-          'click #botonRegistrar': 'registrarNuevo',
-      },
-
-      validar: function(obj){
-          campo = $(obj).val();
-
-          if(!campo.trim()){
-              console.log(campo.trim());
-              alert(mo.validationError);
-              $(obj).focus();
-          }
-      },
-
-      registrarNuevo: function(){
-          alert('Se regsitro');
-
-          nom = $('#txtnombre').val();
-          ape = $('#txtapellido').val();
-          corr = $('#txtcorreo').val();
-          corr2 = $('#txtconfcorreo').val();
-          cla = $('#newpassword').val();
-          fnac = $('#txtfechanac').val();
-          gen = $('input[type=radio]:checked').val()
-
-          mo = new App.Models.RegistroRapido({
-              nombre: nom,
-              apellido: ape,
-              fechaNac: fnac,
-              clave: cla,
-              correo: corr,
-              genero: gen
-          });
-          r = mo.save();
-          // var htm = plantillaModal2()
-
-          this.validar('#txtnombre')
-          this.validar('#txtapellido')
-          this.validar('#txtcorreo')
-          this.validar('#txtconfcorreo')
-          this.validar('#newpassword')
-          this.validar('#txtfechanac')
-
-          // responseJSON.mensaje
-
-      },
-
-      render: function(){
-        this.$el.html(this.plantillaCuerpoIndexParte2(this.model.toJSON()));
-        return this;
-      }
-
-    });
-
-    // Vista para el Pie de pagina del Index:
-    var VistaPiePaginaIndex = Backbone.View.extend({
-      plantillaPiePagina: plantilla('piePaginaPlantilla'),
-
-      initialize: function(){
-        this.render();
-      },
-
-      render: function(){
-        this.$el.html(this.plantillaPiePagina(this.model.toJSON()));
-        return this;
-      }
-
-    });
-
-    // Vista para el Index:
-    App.Views.Index = Backbone.View.extend({
-        tagName: 'body',
-
-        miPlantilla: plantilla('index'),  // _.template($('#personaPlantilla').html()) ,
-
-        initialize: function(){
-        },
-
-        events: {
-          'click #menu-toggle': 'mostrarMenu',
-        },
-
-        mostrarMenu: function(e){
-          e.preventDefault()
-          // this.$('#wrapper').toggleClass('toggled');
-          alert('hola');
-          this.$("#datepicker").datepicker();
-
-        },
-
-        mostrarVitasHijas: function(){
-          miSelector = '#ulMenu';
-          var miVistaCabeceraMenues = new VistaCabeceraMenues({el: miSelector, model: this.model});
-          this.$(miSelector).append(miVistaCabeceraMenues.render().el);
-
-          miSelector = '#divCabeceraIndex';
-          var miVistaCabeceraIndex = new VistaCabeceraIndex({el: miSelector, model: this.model});
-          this.$(miSelector).append(miVistaCabeceraIndex.render().el);
-
-          miSelector = '#ulMenu';
-          var miVistaCabeceraMenu = new VistaCabeceraMenu({el: miSelector, model: this.model});
-          var renderizar = miVistaCabeceraMenu.render().el;
-          this.$(miSelector).append(miVistaCabeceraMenu.render().el);
-
-          miSelector = '#divInicioSesion'
-          var miVistaLogin = new VistaLogin({el: miSelector});
-          this.$(miSelector).append(miVistaLogin.render().el);
-
-          miSelector = '#divPiePaginaIndex';
-          var miVistaPiePaginaIndex = new VistaPiePaginaIndex({el: miSelector, model: this.model});
-          this.$(miSelector).append(miVistaPiePaginaIndex.render().el);
-
-          miSelector = '#divCuerpoIndex'
-          var miVistaCuerpoIndex = new VistaCuerpoIndex({el: miSelector, model: this.model});
-          this.$(miSelector).append(miVistaCuerpoIndex.render().el);
-
-          miSelector = '#divCuerpoIndexParte1'
-          var miVistaCuerpoIndexParte1 = new VistaCuerpoIndexParte1({el: miSelector, model: this.model});
-          console.log(miVistaCuerpoIndexParte1.render().el)
-          this.$(miSelector).append(miVistaCuerpoIndexParte1.render().el);
-
-          miSelector = '#divCuerpoIndexParte2'
-          var miVistaCuerpoIndexParte2 = new VistaCuerpoIndexParte2({el: miSelector, model: this.model});
-          console.log(miVistaCuerpoIndexParte2.render().el)
-          this.$(miSelector).append(miVistaCuerpoIndexParte2.render().el);
-        },
-
-        render: function(){
-          this.$el.html(this.miPlantilla(this.model.toJSON()));
-          return this;
+        for(var i=0; i<longitud; i++){
+            selector = '#'+lista[i].id;
+            descripcion = lista.get(i).placeholder
+            this.validarVacios( selector, descripcion )
+            //alert(mo.validationError);
         }
-    });
+    },
 
-    var indexModelo = new App.Models.Index()
-    var indexView = new App.Views.Index({model: indexModelo});
-    $(document.body).append(indexView.render().el);  // Añade el index al DOM
-    $(document.body).append(indexView.mostrarVitasHijas());  // Añade el el Login al DOM
+    render: function(){
+    this.$el.html(this.plantillaCuerpoIndexParte2(this.model.toJSON()));
+    return this;
+    }
+
+});
+
+// Vista para el Pie de pagina del Index:
+var VistaPiePaginaIndex = Backbone.View.extend({
+    plantillaPiePagina: plantilla('piePaginaPlantilla'),
+
+    initialize: function(){
+    this.render();
+    },
+
+    render: function(){
+    this.$el.html(this.plantillaPiePagina(this.model.toJSON()));
+    return this;
+    }
+
+});
+
+// Vista para el Index:
+App.Views.Index = Backbone.View.extend({
+    tagName: 'body',
+
+    miPlantilla: plantilla('index'),  // _.template($('#personaPlantilla').html()) ,
+
+    initialize: function(){
+    },
+
+    events: {
+        'click #menu-toggle': 'mostrarMenu',
+    },
+
+    mostrarMenu: function(e){
+        e.preventDefault()
+        // this.$('#wrapper').toggleClass('toggled');
+        alert('hola');
+        this.$("#datepicker").datepicker();
+
+    },
+
+    mostrarVitasHijas: function(){
+        miSelector = '#ulMenu';
+        var miVistaCabeceraMenues = new VistaCabeceraMenues({el: miSelector, model: this.model});
+        this.$(miSelector).append(miVistaCabeceraMenues.render().el);
+
+        miSelector = '#divCabeceraIndex';
+        var miVistaCabeceraIndex = new VistaCabeceraIndex({el: miSelector, model: this.model});
+        this.$(miSelector).append(miVistaCabeceraIndex.render().el);
+
+        miSelector = '#ulMenu';
+        var miVistaCabeceraMenu = new VistaCabeceraMenu({el: miSelector, model: this.model});
+        var renderizar = miVistaCabeceraMenu.render().el;
+        this.$(miSelector).append(miVistaCabeceraMenu.render().el);
+
+        miSelector = '#divInicioSesion'
+        var miVistaLogin = new VistaLogin({el: miSelector});
+        this.$(miSelector).append(miVistaLogin.render().el);
+
+        miSelector = '#divPiePaginaIndex';
+        var miVistaPiePaginaIndex = new VistaPiePaginaIndex({el: miSelector, model: this.model});
+        this.$(miSelector).append(miVistaPiePaginaIndex.render().el);
+
+        miSelector = '#divCuerpoIndex'
+        var miVistaCuerpoIndex = new VistaCuerpoIndex({el: miSelector, model: this.model});
+        this.$(miSelector).append(miVistaCuerpoIndex.render().el);
+
+        miSelector = '#divCuerpoIndexParte1'
+        var miVistaCuerpoIndexParte1 = new VistaCuerpoIndexParte1({el: miSelector, model: this.model});
+        console.log(miVistaCuerpoIndexParte1.render().el)
+        this.$(miSelector).append(miVistaCuerpoIndexParte1.render().el);
+
+        miSelector = '#divCuerpoIndexParte2'
+        var miVistaCuerpoIndexParte2 = new VistaCuerpoIndexParte2({el: miSelector, model: this.model});
+        console.log(miVistaCuerpoIndexParte2.render().el)
+        this.$(miSelector).append(miVistaCuerpoIndexParte2.render().el);
+    },
+
+    render: function(){
+        this.$el.html(this.miPlantilla(this.model.toJSON()));
+        return this;
+    }
+});
+
+var indexModelo = new App.Models.Index()
+var indexView = new App.Views.Index({model: indexModelo});
+$(document.body).append(indexView.render().el);  // Añade el index al DOM
+$(document.body).append(indexView.mostrarVitasHijas());  // Añade el el Login al DOM
 
 })();
