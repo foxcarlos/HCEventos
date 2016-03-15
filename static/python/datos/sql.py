bea3216fafb3a07bfc10d852a45fc2accfca2a45
FoxCarlos Trabajo <foxcarlos@gmail.com>
2016-03-15 11:06:28 -0430

    Se movio la clase PG a un archivo separado dentro de la carpeta libs #38

--- static/python/datos/sql.py
+++ static/python/datos/sql.py
@@ -1,63 +1,10 @@
-import psycopg2
-import os
-import time
+from static.python.libs import pgSQL
 import datetime
 
-class pg():
-
-    '''Nueva clase para manejar postgreSQL '''
-
-    def __init__(self):
-        '''Por ahora solo se inicializan variables en el init '''
-
-        self.cur = ''
-        self.conn = ''
-        self.estadoConexion = {'status': 0, 'mensaje': ''}
-        self.cad_conex = ''
-
-    def conectar(self):
-        ''' parametros 1: string "Cadena de conexion"
-        Metodo que permite conectar a postgresql
-        '''
-
-        try:
-            servidor = os.environ['servidor']
-            basedatos = os.environ['basedatos']
-            usuario = os.environ['usuario']
-            clave = os.environ['clave']
-        except:
-            servidor = ''
-            basedatos = ''
-            usuario = ''
-            clave = ''
-
-        string_conn = "host='{0}' dbname='{1}' user='{2}' password='{3}' ".format(servidor, basedatos, usuario, clave)
-        self.cad_conex = string_conn
-
-        try:
-            self.conn = psycopg2.connect(string_conn)
-            self.cur = self.conn.cursor()
-            self.estado = {'status': 1, 'mensaje': 'Conexion Exitosa'}
-        except psycopg2.Error as e:
-            self.estado = {'status': 0, 'mensaje': e.message}
-
-    def ejecutar(self, cadSelect):
-        '''Metodo que permite hacer los Select o Insert a postgresql '''
-
-        # self.estadoConexion = {'status': 0, 'mensaje': ''}
-        cadSelectSql = cadSelect
-
-        if self.estado['status']:
-            try:
-                self.cur.execute(cadSelectSql)
-                self.estado = {'status': 1, 'mensaje': 'Comando Ejecutado con Exito'}
-
-            except psycopg2.Error as e:
-                self.estado = {'status': 0, 'mensaje': e.message}
-# Prueba
 def testPG():
-    ''' '''
-    posg = pg()
+    '''Metodo para Testing de la conexion '''
+
+    posg = pgSQL.PG()
     posg.conectar()
 
     # Si la comnexion a la base de datos falla
@@ -78,7 +25,7 @@ def crearRegRapido(nombre='', apellido='', correo='', clave='', fechanac='', gen
     FechaNac = datetime.datetime.strptime(fechanac.strip(), "%d/%m/%Y").strftime('%Y/%m/%d')
     Genero = genero.strip()
 
-    posg = pg()
+    posg = pgSQL.PG()
     posg.conectar()
 
     # Si la comnexion a la base de datos falla
