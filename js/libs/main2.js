94f61d3c4d3546850fee030e93f15b25b17dec98
FoxCarlos Trabajo <foxcarlos@gmail.com>
2016-04-05 08:59:11 -0430

    actualizando inf de cabecera del evento

--- js/libs/main2.js
+++ js/libs/main2.js
@@ -13,11 +13,11 @@
     App.Models.Index = Backbone.Model.extend({
         defaults:{
             cabeceraTitulo: 'EVENTOS HOSPITAL COROMOTO',
-            jumbotronTitulo: 'II JORNADAS CIENTIFICAS INTEGRALES DEL HOSPITAL COROMOTO',
-            jumbotronAlertaCentral1: 'Del 05 al 07 de Agosto de 2016',
-            jumbotronAlertaCentral2: 'Hotel Venetur',
+            jumbotronTitulo: 'III Congreso IV Jornada Científica Integral y I Exposición Multidisciplinaria e Innovadora Hospital Coromoto 2016',
+            jumbotronAlertaCentral1: 'Del 03 al 06 de Agosto de 2016',
+            jumbotronAlertaCentral2: 'PDVSA La Estancia',
             jumbotronAlertaCentral3: '"Salud con Vocaci&oacute;n Socialista"',
-            jumbotronAlertaCentralDifusion: 'Hotel Venetur Maracaibo',
+            jumbotronAlertaCentralDifusion: 'PDVSA la Estancia Maracaibo',
             jumbotronAlertaCentralContacto1: '(0261) 7900331/7900308/7900364',
             jumbotronAlertaCentralContacto2: 'congresoshospitalcoromoto@gmail.com',
             jumbotronAlertaCentralContacto3: 'congresoshospitalcoromoto@outlook.com',
