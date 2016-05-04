a7f1bc271146997cb25bf7364f0a22aa4cc1768a
foxcarlos <foxcarlos@gmail.com>
2016-05-04 19:13:54 -0430

    se creo la coleccion para el modelo tipo_identiad #67

---
+++ js/colecciones/tipos_identidadCl.js
@@ -0,0 +1,13 @@
+var Collections = Collections || {};
+
+// Modelo usuario
+    Collections.Tipos_identidad = Backbone.Model.extend({
+        urlRoot: 'tipo_identidad',
+
+        model: Models.Tipo_identidad,
+
+        initialize: function(options){
+
+        }
+    });
+
